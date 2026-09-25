0a065aaf366f86b69cc19174589a6e940d16ac84
Edgar Lopez-Murcia <edgarlopez20073103@gmail.com>
2026-09-24 21:30:30 -0500

    Componente tailwind css

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,11 @@
 function App() {
   return (
-    <>
+    <main className="max-w-6xl mx-auto px-6 py-10 flex flex-col gap-10">
       <nav className="flex items-center justify-between px-8 py-4 bg-white border-b border-slate-200">
         <div className="text-xl font-extrabold text-verde">
           TechStore Pro
         </div>
-        <ul className="flex gap-6 text-sm font-semibold text-texto-dim">
+        <ul className="hidden md:flex gap-6 text-sm font-semibold text-texto-dim">
           <li>Inicio</li>
           <li>Producto</li>
           <li>Nosotros</li>
@@ -15,22 +15,46 @@ function App() {
           Ingresar
         </button>
       </nav>
-      <div className="flex flex-col gap-2 p-4 rounded-xl shadow-md max-w-xs">
-        <img src="https://placehold.co/300x200" className="rounded-lg" />
-        <h3 className="font-bold text-lg">Mouse Inalámbrico</h3>
-        <p className="text-verde font-extrabold">$89.900</p>
-        <button className="bg-transparent text-verde border-2 border-verde py-3 px-6 rounded-xl font-bold">
-        Ver más detalles
-        </button>
-      </div>
-      <button className="bg-transparent text-verde border-2 border-verde py-3 px-6 rounded-xl font-bold">
-        Ver más detalles
-      </button>
-      <footer className="flex justify-between items-center p-6 bg-texto text-white">
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+
+        {/* Tarjeta 1 */} 
+        <div className="flex flex-col gap-2 p-4 rounded-xl shadow-md bg-white">
+          <img src="https://placehold.co/300x200" className="rounded-lg" />
+          <h3 className="font-bold text-lg">Mouse Inalámbrico</h3>
+          <p className="text-texto-dim text-sm">Mouse ergonómico, Conexión Bluetooth</p>
+          <p className="text-verde font-extrabold">$89.900</p>
+        </div>
+
+        {/* Tarjeta 2 */} 
+        <div className="flex flex-col gap-2 p-4 rounded-xl shadow-md bg-white">
+          <img src="https://placehold.co/300x200" className="rounded-lg" />
+          <h3 className="font-bold text-lg">Teclado Mecánico</h3>
+          <p className="text-texto-dim text-sm">Switches azules, Retroiluminado RGB</p>
+          <p className="text-verde font-extrabold">$149.900</p>
+        </div>
+
+        {/* Tarjeta 3 */} 
+        <div className="flex flex-col gap-2 p-4 rounded-xl shadow-md bg-white">
+          <img src="https://placehold.co/300x200" className="rounded-lg" />
+          <h3 className="font-bold text-lg">Moonitor 24</h3>
+          <p className="text-texto-dim text-sm">Full  HD, 75Hz, panel IPS</p>
+          <p className="text-verde font-extrabold">$899.900</p>
+        </div>
+
+        {/* Tarjeta 4 */} 
+        <div className="flex flex-col gap-2 p-4 rounded-xl shadow-md bg-white">
+          <img src="https://placehold.co/300x200" className="rounded-lg" />
+          <h3 className="font-bold text-lg">Audífonos Bluethooth</h3>
+          <p className="text-texto-dim text-sm">Cancelación de ruido, 20h de batería</p>
+          <p className="text-verde font-extrabold">$199.900</p>
+        </div>
+
+      </section>
+      <footer className="flex flex-col md:flex-row justify-between items-center gap-2 md:gap-0 p-6 bg-texto text-white">
         <p>© 2026 TechStore Pro</p>
         <p className="text-sm">Hecho con Tailwind CSS</p>
       </footer>
-    </>
+    </main>
   )
 }
 export default App
